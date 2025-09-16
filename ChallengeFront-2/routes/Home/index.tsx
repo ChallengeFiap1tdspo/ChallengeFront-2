785f0817d2edf9ce50762db06c38c93334b3c0e9
phestalise <phestalise@gmail.com>
2025-09-16 19:33:48 -0300

    ajustando conteudos na home

--- ChallengeFront-2/routes/Home/index.tsx
+++ ChallengeFront-2/routes/Home/index.tsx
@@ -2,7 +2,8 @@ export default function Home(){
 
     return(
         <main>
-            <h1> HOME</h1>
+            <h1> Seja bem-vindo à Teleconsulta do Hospital das Clínicas </h1>
+            <p>Estamos aqui para oferecer o suporte que você precisa para sua consulta online.</p>
         </main>
     );
 }
