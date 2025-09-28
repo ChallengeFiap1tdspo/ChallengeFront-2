37cfd797794f3797844c607046b07e3bb4c7cc8a
phestalise <phestalise@gmail.com>
2025-09-27 22:07:19 -0300

    aletações

--- ChallengeFront-2/src/routes/Faq.tsx
+++ ChallengeFront-2/src/routes/Faq.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import "..global.css";
-// import { Link } from "react-router-dom";
+import "../../src/";
+import { Link } from "react-router-dom";
 
 const faqsHTML = [
   {
